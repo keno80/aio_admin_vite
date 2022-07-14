25d28a780ce678e58b657c427b659831f2695a3e
keno80 <1211582428@qq.com>
2022-07-14 11:35:53 +0800

    fix: 去除home主页控制黑暗模式

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { toggleDark } from '@/composables/dark'
+
 </script>
 
 <template>
   <div>
-    <el-button type="primary" @click="toggleDark()">黑暗模式</el-button>
+    <el-button type="primary">黑暗模式</el-button>
   </div>
 </template>
