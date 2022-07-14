230c7392728b08de20ae23585cb7f0d914b596dc
keno80 <1211582428@qq.com>
2022-07-14 13:28:25 +0800

    feat: 添加全屏操作以及修改结构

--- src/components/Layout/Header.vue
+++ src/components/Layout/Header.vue
@@ -1,11 +1,25 @@
 <script lang="ts" setup>
+import { ref } from 'vue';
 import { toggleDark } from '@/composables/dark'
 import { appStore } from '@/store/app';
+import { storeToRefs } from 'pinia';
 
-const store = appStore()
+const { sidebarCollapse } = storeToRefs(appStore())
+const isFull = ref<Boolean>(false)
 
 const handleSidebarCollapse = () => {
-  store.sidebarCollapse = !store.sidebarCollapse
+  sidebarCollapse.value = !sidebarCollapse.value
+}
+
+const fullScreen = () => {
+  let el = document.documentElement
+  if (document.fullscreenElement) {
+    document.exitFullscreen()
+    isFull.value = false
+  } else {
+    el.requestFullscreen()
+    isFull.value = true
+  }
 }
 
 </script>
@@ -14,14 +28,14 @@ const handleSidebarCollapse = () => {
   <div class="header" h-60px>
     <div class="header-content" flex h-50px mt-20px mx-16px>
       <div class="left" bg-white dark:bg-black-22 h-40px px-20px mr-16px rounded-4px>
-        <div class="i-line-md:menu-fold-right" h10 cursor-pointer hover:text-indigo @click="handleSidebarCollapse"
-          v-if="store.sidebarCollapse"></div>
-        <div class="i-line-md:menu-fold-left" h10 cursor-pointer hover:text-indigo @click="handleSidebarCollapse"
-          v-else></div>
+        <div :class="[sidebarCollapse ? 'i-line-md:menu-fold-right' : 'i-line-md:menu-fold-left']" h10 cursor-pointer
+          hover:text-indigo @click="handleSidebarCollapse()"></div>
       </div>
-      <div class="right" bg-white dark:bg-black-22 ml-auto h-40px px-20px rounded-4px>
+      <div class="right" bg-white dark:bg-black-22 ml-auto h-40px rounded-4px flex>
+        <div :class="[isFull ? 'i-fluent:full-screen-minimize-24-filled' : 'i-fluent:full-screen-maximize-24-filled']"
+          cursor-pointer hover:text-indigo @click="fullScreen()" style="height: 2.5em" mx-3></div>
         <div i-line-md:sunny-outline-twotone-loop dark:i-line-md-moon-twotone cursor-pointer hover:text-indigo
-          @click="toggleDark()" style="height: 2.5em"></div>
+          @click="toggleDark()" style="height: 2.5em" mx-3></div>
       </div>
     </div>
 
