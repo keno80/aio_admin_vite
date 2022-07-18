bb707aa40040c427de94b0da4986eb25df972451
keno80 <1211582428@qq.com>
2022-07-18 18:24:02 +0800

    feat: 添加tagsview右键菜单

---
+++ src/components/Layout/TagsView/context.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const props = defineProps({
+  options: {
+    type: Object,
+    default: () => { }
+  }
+})
+</script>
+
+<template>
+  <div class="dropdown" v-show="props.options.show">
+    下拉菜单
+  </div>
+</template>
