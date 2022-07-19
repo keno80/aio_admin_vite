db638806a6c8f8abb2a0e7af5778dd13eb0eef63
keno80 <1211582428@qq.com>
2022-07-19 18:07:49 +0800

    feat: 添加reload页面操作

--- src/store/app.ts
+++ src/store/app.ts
@@ -1,11 +1,19 @@
 import { defineStore } from "pinia";
+import { nextTick } from "vue";
 
 export const appStore = defineStore('appStore', {
   state: () => {
     return {
-      sidebarCollapse: false
+      sidebarCollapse: false,
+      isReload: true
     }
   },
   getters: {},
-  actions: {}
+  actions: {
+    async reload() {
+      this.isReload = false
+      await nextTick()
+      this.isReload = true
+    }
+  }
 })
